8c8d22f526aeda4de3bac22c83675d683eda1fe6
Ali Alhusseini <hello@alialhusseini.com>
2023-11-09 19:24:42 -0400

    Resolve conflicts

--- scripts/script.js
+++ scripts/script.js
@@ -73,27 +73,15 @@ for (i = 0; i < acc.length; i++) {
 /* End About script */
 
 /* Start Get Involved */
-const donateButton = document.getElementById('donate-button');
-        const volunteerButton = document.getElementById('volunteer-button');
-        const donationBox = document.querySelector('.donation-box');
-        const volunteerOpportunities = document.querySelector('.vol-box');
-
 const donateButton = document.getElementById("donate-button");
 const volunteerButton = document.getElementById("volunteer-button");
 const donationBox = document.querySelector(".donation-box");
 const volunteerOpportunities = document.querySelector(".vol-box");
 
-
 donateButton.addEventListener("click", () => {
   donationBox.classList.remove("hidden");
   volunteerOpportunities.classList.add("hidden");
 });
-
-
-        volunteerButton.addEventListener('click', () => {
-            donationBox.classList.add('hidden');
-            volunteerOpportunities.classList.remove('hidden');
-        });
 
 volunteerButton.addEventListener("click", () => {
   donationBox.classList.add("hidden");
